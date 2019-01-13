379ff809266c09b34d0f7bc23dab21bf3cd1b5e2
Sam <samgordondev@gmail.com>
2019-01-12 19:01:17 -0600

    passed test for collapsible

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 
  var projectName = 'technical-docs-page';
  localStorage.setItem('example_project', 'Technical Docs Page');
+
+
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
